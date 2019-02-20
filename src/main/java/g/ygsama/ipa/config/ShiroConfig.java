ae217e3ebfcacd5ef432879114c310d775832356
ygsama <ygsama@qq.com>
2019-02-20 09:05:37 +0800

    修改shiro配置

--- src/main/java/g/ygsama/ipa/config/ShiroConfig.java
+++ src/main/java/g/ygsama/ipa/config/ShiroConfig.java
@@ -32,12 +32,19 @@ public class ShiroConfig {
 
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+
         DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
-        // 由于demo1展示统一使用注解做访问控制，所以这里配置所有请求路径都可以匿名访问
-        chain.addPathDefinition("/**", "anon"); // all paths are managed via annotations
+
+        //哪些请求可以匿名访问
+        chain.addPathDefinition("/user", "anon");
+        chain.addPathDefinition("/login", "anon");
+        chain.addPathDefinition("/index", "anon");
+
+
+        //除了以上的请求外，其它请求都需要登录
+        chain.addPathDefinition("/**", "authc");
 
         // 这另一种配置方式。但是还是用上面那种吧，容易理解一点。
-        // or allow basic authentication, but NOT require it.
         // chainDefinition.addPathDefinition("/**", "authcBasic[permissive]");
         return chain;
     }
