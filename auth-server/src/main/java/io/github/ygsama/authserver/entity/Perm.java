7141bf5a2a152c0220bf22a2a50ec1c95645a557
ygsama <ygsama@qq.com>
2019-02-21 09:39:58 +0800

    添加实体

---
+++ auth-server/src/main/java/io/github/ygsama/authserver/entity/Perm.java
@@ -0,0 +1,75 @@
+package io.github.ygsama.authserver.entity;
+
+
+/**
+ * 权限
+ */
+public class Perm {
+
+    /**
+     * 权限类型：菜单
+     */
+    public static int PTYPE_MENU = 1;
+    /**
+     * 权限类型：按钮
+     */
+    public static int PTYPE_BUTTON = 2;
+
+    private String pid;       // 权限id
+    private String pname;   // 权限名称
+    private String ptype;  // 权限类型：1.菜单；2.按钮
+    private String pvalue;    // 权限值，shiro的权限控制表达式
+    private String created;   // 创建时间
+    private String updated;   // 修改时间
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPtype() {
+        return ptype;
+    }
+
+    public void setPtype(String ptype) {
+        this.ptype = ptype;
+    }
+
+    public String getPvalue() {
+        return pvalue;
+    }
+
+    public void setPvalue(String pvalue) {
+        this.pvalue = pvalue;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+
+
+}
